ee4ae0b3630cb213bd28195e2a7901b2032301ce
Devjyoti Roy <devjyotiroy05@gmail.com>
2026-02-12 16:14:05 +0530

    Update Scanner.js

--- frontend/src/components/Scanner.js
+++ frontend/src/components/Scanner.js
@@ -35,7 +35,7 @@ function Scanner({ user, onLogout }) {
     
     try {
       const token = localStorage.getItem("token");
-      const res = await axios.post("http://localhost:5000/api/scan", { url }, {
+      const res = await axios.post("https://truthtrace.onrender.com/api/scan", { url }, {
         headers: { Authorization: `Bearer ${token}` },
         timeout: 60000
       });
